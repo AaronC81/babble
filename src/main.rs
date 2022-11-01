dfb7ba853bb26a923c6a88a028f8c9a55edc53dc
Aaron Christiansen <aaronc20000@gmail.com>
2022-11-01 22:46:24 +0000

    Use clap instead

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 
 use std::{env::args, fs::read_to_string};
 
+use clap::Parser;
 use interpreter::Interpreter;
 use source::SourceFile;
 
@@ -15,22 +16,42 @@ mod parser;
 mod interpreter;
 mod doc_gen;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Path to a script to run
+   #[arg(short = 'f', long, conflicts_with = "code")]
+   file: Option<String>,
+
+   /// Raw code to run
+   #[arg(short = 'e', long, conflicts_with = "file")]
+   code: Option<String>,
+
+   /// Rather than running any code, print Markdown stdlib documentation
+    #[arg(long, conflicts_with = "code", conflicts_with = "file")]
+    doc_gen: bool,
+}
+
 fn main() {
-    let arg = args().nth(1).expect("no code passed");
+    let args = Args::parse();
+
     let input_contents;
     let input_name;
-    if arg == "--file" || arg == "-f" {
-        input_name = args().nth(2).expect("no file passed");
+    if let Some(file) = args.file {
+        input_name = file.clone();
         input_contents = read_to_string(input_name.clone()).unwrap();
-    } else if arg == "--doc-gen" {
+    } else if let Some(code) = args.code {
+        input_name = "command-line".to_string();
+        input_contents = code.clone();
+    } else if args.doc_gen {
         let interpreter = Interpreter::new().unwrap();
         let output = doc_gen::generate_documentation(&interpreter);
         println!("{}", output);
         return;
     } else {
-        input_name = "command-line".to_string();
-        input_contents = arg.clone();
-    }
+        unreachable!("invalid command")
+    };
+
     let src = SourceFile::new(&input_name, &input_contents).rc();
     if let Err(e) = Interpreter::new().map(|mut i| i.parse_and_evaluate(src)).flatten() {
         println!("Fatal error:\n  {}\n", e.kind);
